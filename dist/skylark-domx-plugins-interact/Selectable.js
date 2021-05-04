cf0a71c42da504f10385dc1eefa5ecd14fa83411
hudaokeji <github@hudaokeji.com>
2021-05-04 15:30:07 +0900

    update

--- dist/skylark-domx-plugins-interact/Selectable.js
+++ dist/skylark-domx-plugins-interact/Selectable.js
@@ -1,5 +1,5 @@
 /**
- * skylark-domx-interact - The interact features enhancement for dom.
+ * skylark-domx-plugins-interact - The interact features enhancement for dom.
  * @author Hudaokeji Co.,Ltd
  * @version v0.9.0
  * @link www.skylarkjs.org
---
+++ dist/skylark-domx-plugins-interact/interact.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-domx-plugins-interact - The interact features enhancement for dom.
+ * @author Hudaokeji Co.,Ltd
+ * @version v0.9.0
+ * @link www.skylarkjs.org
+ * @license MIT
+ */
+define(["skylark-langx/skylark","./polyfill"],function(a){return a.attach("domx.interact",{})});
+//# sourceMappingURL=sourcemaps/interact.js.map
